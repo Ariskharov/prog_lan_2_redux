1df141ef3e4af518db057a2764314447585be389
Ariskharov <lirov_a@iuva.kg>
2026-01-26 21:35:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <>
+
+      <Header />
+      <Home />
+      <Footer />
+
+    </>
+  )
+}
+
+export default App
